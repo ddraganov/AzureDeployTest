c4d7a336dee9c010531b92b4fb58aee6409d13e3
Dragomir Draganov <dragomir.draganov@bedegaming.com>
2016-12-05 22:57:31 +0200

    q

---
+++ AzureDeployTest/Controllers/SomethingsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AzureDeployTest.Models;
+
+namespace AzureDeployTest.Controllers
+{
+    public class SomethingsController : ApiController
+    {
+        [HttpPost]
+        public void Create(Something something)
+        {
+            // TODO
+        }
+
+        [HttpGet]
+        public void GetAll()
+        {
+            // TODO
+        }
+    }
+}
---
+++ AzureDeployTest/Models/Something.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AzureDeployTest.Models
+{
+    public class Something
+    {
+        public string Name { get; set; }
+    }
+}
